be584b70cca363ea96788ef308b6889fc0559b01
panis <neatLine@gmail.com>
2016-09-28 08:42:25 +0800

    areyouok

--- tmpHomework/1point17.c
+++ tmpHomework/1point17.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
-FILE * getFile() {
-	FILE * fp;
-	if ((fp=fopen("test","w"))==NULL) {
-		printf("File cannot be opened");
+int Fibonacci(int k, int m)
+{
+	if (m < k - 1) {
+		return 0;
+	}
+	else if (m == k - 1) {
+		return 1;
+	}
+	else {
+		int pre = 1;
+		int next = 1;
+		m = m - k;
+		while (m > 0) {
+			next += pre;
+			pre = next - pre;
+			m--;
+		}
+		return next;
 	}
 }
 
 int main()
 {
-	;
+	printf("%d",Fibonacci(2,10));
 	return 0;
 }
