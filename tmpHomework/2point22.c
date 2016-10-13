bf499aa08216b053e54836cb1684805841ace8fb
test_opencv_gui <neatlinefu@gmail.com>
2016-10-13 17:01:40 +0800

    Update 2point22.c

--- tmpHomework/2point22.c
+++ tmpHomework/2point22.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-typedef struct LNode {
+typedef struct LNode {	//定义节点
 	int Data;
 	LNode next;
 }
 
-LNode changeList(*LinkList head) {
+LNode changeList(*LinkList head) {//逆序
 	LNode p = head;  
     LNode q = p.next;
 	LNode r;
@@ -17,4 +17,21 @@ LNode changeList(*LinkList head) {
 	head.next = null;  
 	head = p;  
 	return head;
+}
+
+int main()
+{
+	LinkList ll = (LinkList)malloc(sizeof(LNode));
+	LNode temp;
+	temp = (LNode)malloc(sizeof(LNode));
+	ll->next=temp;
+	temp->Data=1;
+	temp->next=(LNode)malloc(sizeof(LNode));
+	temp=temp->next;
+	temp->data=10;
+	temp->next=(LNode)malloc(sizeof(LNode));
+	temp=temp->next;
+	temp->data=100;
+	temp->next=NULL;
+	LinkList ge = changeList(ll);
 }
