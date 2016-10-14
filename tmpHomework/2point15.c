efae4fdccbcdee63ce9d497245fd21b327e14345
test_opencv_gui <neatlinefu@gmail.com>
2016-10-14 08:20:38 +0800

    Update 2point15.c

--- tmpHomework/2point15.c
+++ tmpHomework/2point15.c
@@ -1,11 +1,23 @@
-#include <stdio.h>
-typedef struct LNode {
-	int Data;
-	LNode next;
-} LNode,*LinkList;
+#include <iostream>
 
-LNode linkNode(*LinkList ha, *LinkList hb, int m, int n) {
-	*LinkList temp = ha;
+using namespace std;
+
+template<typename T>
+class Node
+{
+public:
+    T Data;
+    Node<T>* Next;
+    Node<T>()
+    {
+        Next = nullptr;
+    }
+};
+
+
+
+Node<int>* linkNode(Node<int>* ha, Node<int>* hb, int m, int n) {
+	Node<int>* temp = ha;
 	if(m < n) {
 		ha = hb;
 		hb = temp;
@@ -17,4 +29,34 @@ LNode linkNode(*LinkList ha, *LinkList hb, int m, int n) {
 	hb->next = ha->next;
 	free(ha);
 	return temp;
+}
+
+void printList(Node<int> * hc)
+{
+    while (hc != nullptr)
+    {
+        cout << hc->Data << "  ";
+        hc = hc->Next;
+    }
+}
+
+
+int main()
+{
+	Node<int>* ha = new Node<int>();
+	ha->Data = 1;
+	ha->Next = new Node<int>();
+	ha->Next->Data = 0;
+	ha->Next->Next = NULL;
+	Node<int>* hb = new Node<int>();
+	hb->Date = 3;
+	hb->Next = new Node<int>();
+	hb->Next->Data = 4;
+	hb->Next->Next = new Node<int>();
+	hb->Next->Next->Data=2;
+	hb->Next->Next->Next=NULL;
+	Node<int>* hc = LinkNode(ha,hb,2,3);
+	printList(hc);
+	
+	
 }
