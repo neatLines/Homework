4c2c639b1b162382d31b8b5a15650842b4c784ce
test_opencv_gui <neatlinefu@gmail.com>
2016-10-14 08:35:13 +0800

    Update 2point15.c

--- tmpHomework/2point15.c
+++ tmpHomework/2point15.c
@@ -1,39 +1,33 @@
-//
-//  main.cpp
-//  homeworkcpp
-//
-//  Created by panis on 2016/10/14.
-//  Copyright © 2016年 panis. All rights reserved.
-//
 #include <iostream>
 
 using namespace std;
-//定义节点
+
 template<typename T>
 class Node
 {
 public:
     T Data;
     Node<T>* Next;
+    Node<T>()
+    {
+        Next = nullptr;
+    }
 };
-
-
-//链接两链表，
-//链表1，链表2，链表1长度，链表2长度
-Node<int>* linkNode(Node<int>* ha, Node<int>* hb, int m, int n) {
-    Node<int>* temp = ha;
-    if(m < n) {
-        ha = hb;
-        hb = temp;
+Node<int>* changeList(Node<int>* head) {//逆序
+    Node<int>* p = head;
+    Node<int>* q = p->Next;
+    Node<int>* r;
+    while(q != nullptr){
+        r = q->Next;
+        q->Next = p;
+        p = q;
+        q = r;
     }
-    temp = hb;
-    while (hb->Next!=NULL) {
-        hb = hb->Next;
-    }
-    hb->Next = ha;
-    free(ha);
-    return temp;
+    head->Next = nullptr;
+    head = p;
+    return head;
 }
+
 //打印链表
 void printList(Node<int> * hc)
 {
@@ -52,14 +46,7 @@ int main()
     ha->Next = new Node<int>();
     ha->Next->Data = 0;
     ha->Next->Next = NULL;
-    Node<int>* hb = new Node<int>();
-    hb->Data = 3;
-    hb->Next = new Node<int>();
-    hb->Next->Data = 4;
-    hb->Next->Next = new Node<int>();
-    hb->Next->Next->Data=2;
-    hb->Next->Next->Next=NULL;
-    Node<int>* hc = linkNode(ha,hb,2,3);
+    Node<int>* hc = changeList(ha);
     printList(hc);
     
     
