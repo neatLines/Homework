cbd79f518aa311d8f5a6cbc9b089ae3106f850e8
neatLines <neatlinefu@gmail.com>
2016-11-18 22:34:33 +0800

    add 6.60

---
+++ tmpHomework/6point60.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+typedef struct tree_node{
+    int elem;
+    tree_node *brother;
+    tree_node *child;
+}tree_node,*tree;
+
+tree_node* createNode(int elem) {
+    tree_node *node = new tree_node;
+    node->elem=elem;
+    node->brother = nullptr;
+    node->child = nullptr;
+    return node;
+}
+
+tree newATree() {
+    tree root = createNode(1);
+    root->child = createNode(3);
+    root->child->brother = createNode(4);
+    root->child->child = createNode(5);
+    root->child->brother->brother = createNode(6);
+    root->child->brother->child = createNode(12);
+    root->child->brother->child->brother = createNode(14);
+
+    return root;
+}
+
+int getNumberOfLeaf(tree root) {
+    if (root == nullptr) {
+        return 1;
+    }
+    int count = getNumberOfLeaf(root->child);
+    if (root->brother != nullptr) {
+        count += getNumberOfLeaf(root->brother);
+    }
+    return count;
+}
+
+int main() {
+    std::cout << getNumberOfLeaf(newATree()) << std::endl;
+    return 0;
+}
