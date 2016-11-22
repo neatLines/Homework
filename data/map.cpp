1636eeaa77672672a36bd2358f391390cf832047
neatLines <neatlinefu@gmail.com>
2016-11-22 17:56:57 +0800

    change map

--- data/map.cpp
+++ data/map.cpp
@@ -11,35 +11,44 @@
 #define MAXV 50
 #define MaxSize 1000
 #define INF 32767
+
+
 using namespace std;
+
+typedef struct Node {
+
+};
+
 //邻接矩阵存储方法
+
 typedef struct MGraph{
     int edges[MAXV][MAXV];
     int n;
-    char vexs[MAXV];
 } MGraph;
 
-int getMinNumberIndex(int in[],int n){
-    int temp = INF;
-    for (int i = 0 ; i < n ; i++) {
-        if (in[i]<temp) {
-            temp = i;
-        }
-    }
-    return temp;
-}
+
 
 //迪杰斯特拉
 int getLength(MGraph g,int e) {
     int length = 0;
-    int hadGot[MAXV] = {0};
-    hadGot[e]=0;
-    int temp = INF;
+    int dist[MAXV] = {INF};
+    bool hadGot[MAXV] = {0};
+    hadGot[e] = 1;
     for (int i = 0; i < g.n ; i++) {
-        int tempMin = getMinNumberIndex(g.edges[e], g.n);
-        
+        int temp = INF;
+        for (int j = 0; j < g.n; j++) {
+            if (hadGot[j]) {
+                for (int k = 0; k < g.n; ++k) {
+                    if ( temp > g.edges[j][k]&&!hadGot[k]) {
+                        temp = g.edges[j][k];
+                        e = k;
+                    }
+                }
+            }
+        }
+        hadGot[e]=1;
+        length+=temp;
     }
-    
     return length;
 }
 
@@ -49,9 +58,9 @@ int getMinLength(MGraph g) {
     int Length[MAXV] = {INF};
     for (int i = 0; i < g.n ; i++) {
         int tempLength = INF;
-        
+
         tempLength = getLength(g,i);
-        
+
         if (tempLength<Length[minPoint]) {
             minPoint = i;
         }
@@ -71,7 +80,7 @@ int main()
         /*
          32767 5 8 7 32767 3
          5 32767 4 32767 32767 32767
-         8 4 32767 5   9
+         8 4 32767 5 32767 9
          7 32767 5 32767 5 6
          32767 32767 32767 5 32767 1
          3 32767 9 6 1 32767
@@ -83,7 +92,7 @@ int main()
         }
         g.n=n;
         cout << ("采用迪杰斯特拉算法得到的地址为:") << getMinLength(g) << endl;
-        
+
         printf("\n请输入带权无向图的顶点个数：");
     }
     return 0;
